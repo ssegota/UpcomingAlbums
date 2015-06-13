b03d11742125bb4c9c008ff8ba57c3bbc83fdbc6
ssegota <ssegota@gmail.com>
2015-06-13 23:08:52 +0200

    Done with Search.

--- bin/db_api.rb
+++ bin/db_api.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby
+
 require 'sinatra'
 require 'mysql'
 
@@ -7,44 +9,48 @@ set :static, true
 set :public_folder, "static"
 set :views, "views"
 
+#mysql setup
+con = Mysql.new 'localhost', 'root', 'sandisegota1995', 'albums'
+
 #start values setup
 default_value = ''
-
+topTableDef="asdasdasdads"
 #selectquery 
 
 def queryAdd(query, standardQuery, attributeName, queryAddition)
 		
  	default_value = ''	
 	if queryAddition != default_value
-		if query != standardQuery
-			query += " AND "
-		end
-		query += "(#{attributeName} = #{queryAddition})"
+		#if query != standardQuery
+		#	query += " AND "
+		#end
+		query += " AND (#{attributeName} = '#{queryAddition}')"
 	end
 	return query
 end
 
 def createSelectQuery(band, location, album, outTime, genre, subGenre)
-	selectQueryBase = "SELECT * FROM musicians WHERE "
+	selectQueryBase = "SELECT name, albumname, genre, subgenre, nofsongs, date, location FROM album, musician WHERE (authorID=bandID) "
 	query=selectQueryBase
 	default_value = ''
 
 	if band != default_value
-		query += "(band = #{band})"
+		query += "AND (name = '#{band}')"
 	end
 	
 	query = queryAdd(query, selectQueryBase, "location", location)
-	query = queryAdd(query, selectQueryBase, "album", album)
+	query = queryAdd(query, selectQueryBase, "albumname", album)
 	query = queryAdd(query, selectQueryBase, "outTime", outTime)
 	query = queryAdd(query, selectQueryBase, "genre", genre)
 	query = queryAdd(query, selectQueryBase, "subGenre", subGenre)
-
+	query += ";"
 	return query
 end
 
 #Landing page ERB
 get '/' do
-   	erb :form	    
+	
+   	erb :form,:locals => {'top' => topTableDef}	    
 end
 
 #POST ERB
@@ -58,6 +64,20 @@ post '/' do
 	subGenre = params[:subGenre] || default_value
 
 	query = createSelectQuery(band, location, album, outTime, genre, subGenre)
+	puts query	
+	results=con.query(query)	
+	n_rows=results.num_rows
 	
-	erb:index,:locals => {'query' => query}
+	forPrint = ""
+	puts "There are #{n_rows} rows in the result set"
+	results.each_hash do |row|
+       		#puts row['location'] + " " +row['name']
+    		forPrint+="<tr>"+"<td>"+row['name'] + "</td>"+"<td>" +row['albumname'] + "</td>"+"<td>" + row['genre'] + "</td>"+"<td>" + row['subgenre'] +"</td>"+"<td>" + row['nofsongs'] + "</td>"+"<td>" + row['date'] +"</td>"+"<td> " + row['location'] +" <\td>" + "</tr>"
+	end    
+	puts forPrint
+	erb:index,:locals => {'query' => forPrint}
+	
 end
+
+
+
